65ecad42630cbb792b77a9736a1ad7ae501dc03d
lucasdias <lucaswinyb@gmail.com>
2022-05-20 21:12:09 -0300

    Carrinho Produtos

--- views-cache/index.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/index.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -36,7 +36,7 @@
 								Apple <span class="primary">Store <strong>Ipod</strong></span>
 							</h2>
 							<h4 class="caption subtitle">& Phone</h4>
-							<a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+							<a class="caption button-radius" href=""><span class="icon"></span>Comprar</a>
 						</div>
 					</li>
 				</ul>
@@ -89,7 +89,7 @@
                                 <div class="product-f-image">
                                     <img src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="">
                                     <div class="product-hover">
-                                        <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i>Comprar</a>
+                                        <a href="/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/add" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i>Comprar</a>
                                         <a href="products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" class="view-details-link"><i class="fa fa-link"></i>Ver detalhes</a>
                                     </div>
                                 </div>
